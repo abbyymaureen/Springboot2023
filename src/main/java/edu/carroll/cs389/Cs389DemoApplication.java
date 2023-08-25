8843b78bdf50301fe3cadeee73232b8eb336fa5e
Abby Brown <abrown01@carroll.edu>
2023-08-25 10:18:55 -0600

    Initial commit

---
+++ src/main/java/edu/carroll/cs389/Cs389DemoApplication.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs389;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs389DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs389DemoApplication.class, args);
+	}
+
+}
