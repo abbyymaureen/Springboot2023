13bcc6f1a91c8275eed8b1295275a89d9d653564
Abby Brown <abrown01@carroll.edu>
2023-08-28 10:34:17 -0600

    Rename Stuff

--- src/test/java/edu/carroll/cs389/IndexControllerTest.java
+++ src/test/java/edu/carroll/cs389/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package edu.carroll.cs389;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsString;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -11,15 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HelloController.class)
-public class HelloControllerTest {
+@WebMvcTest(IndexController.class)
+public class IndexControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void indexTest() throws Exception {
-        mockMvc.perform(get("/?name=random")).andDo(print())
+        mockMvc.perform(get("/")).andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("<!DOCTYPE HTML>\n<html>\n<head>\n    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n    <title>Serving HTML Content</title>\n</head>\n<body>\n<p>Hello, random!</p>\n</body>\n</html>")));
+                .andExpect(content().string(containsString("Welcome to the application!")))
+                .andExpect(content().string(containsString("login")));
     }
 }
